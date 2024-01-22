db35db9973265ea9d4f5cb536d724447c7781ea8
Harvmaster <hzuccon@gmail.com>
2024-01-22 17:57:54 +1100

    initial commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+# SolarMQTT
+Simple test to read the realtime data of Victron CCGX Device
+
+# Config 
+```ts
+const config = {
+  vrm: {
+    ip: 'Your CCGX devices local ip address'
+    port: 1883 // This may need to change, but i found this port to work
+  },
+  socketio: {
+    url: 'URL to socket.io server'
+  }
+}
+
+export default config
+```
+
+# Useful Links
+- [Describes how to connect to mqtt](https://community.victronenergy.com/questions/155407/mqtt-local-via-mqtt-broker.html)
+- [Example Connection Code](https://community.victronenergy.com/questions/135757/mqtt-jsts-implementation.html)
---
+++ src/index.ts
@@ -0,0 +1,123 @@
+import MQTT, { MqttClient } from 'mqtt'
+import socketio, { Socket } from 'socket.io-client'
+
+import config from '../config'
+
+class SolarDataCollector {
+  #socket?: Socket
+  #mqtt?: MqttClient
+  #id?: string
+
+  constructor () {
+
+  }
+
+  async init() {
+    this.#socket = await this.connectSocket()
+    this.#mqtt = await this.connectMqtt()
+  }
+
+  connectSocket (): Promise<Socket> {
+    return new Promise((resolve, reject) => {
+      const socket = socketio(config.socketio.url)
+
+      socket.on('connect', () => {
+        console.log('connected to socket.io')
+        socket.emit('join', {
+          clientType: 'provider',
+          data: {
+            providerId: Math.floor(Math.random() * 10000000)
+          }
+        })
+        resolve(socket)
+      })
+      socket.on('connect_error', (err) => {
+        console.log('error connecting to socket.io', err)
+        reject(err)
+      })
+    })
+  }
+
+  connectMqtt (): Promise<MqttClient> {
+    return new Promise((resolve, reject) => {
+      const client = MQTT.connect(`mqtt://${config.vrm.ip}:${config.vrm.port}`)
+      client.on('connect', () => {
+        console.log('connected')
+        client.subscribe('#', (err) => {
+          if (err) {
+            console.log('error subscribing', err)
+          }
+        })
+        resolve(client)
+      })
+
+      // Create listener to call keepalive. This is what triggers data to be received
+      client.once('message', (topic, message) => {
+        const topicString = topic.toString()
+        const messageString = message.toString()
+        if (topicString.endsWith('system/0/Serial')) {
+          const msg = JSON.parse(messageString)
+          this.#id = msg.value
+          this.keepAlive()
+        }
+      })
+    })
+  }
+
+  keepAlive () {
+    setInterval(() => {
+      if (!this.#mqtt) throw new Error('MQTT not initialized')
+      if (!this.#id) throw new Error('ID not initialized')
+
+      this.#mqtt.publish(`R/${this.#id}/keepalive`, '')
+    }, 10000)
+  }
+
+  handleMessage () {
+    if (!this.#mqtt) throw new Error('MQTT not initialized')
+    
+    this.#mqtt.on('message', (topicRaw, messageRaw) => {
+      if (!this.#socket) throw new Error('Socket not initialized')
+      
+      const topic = topicRaw.toString()
+      const message = messageRaw.toString()
+
+      const topicParts = topic.split('/')
+
+      // topics we want
+      // N/$id/system/0/Dc/:device/:value
+      // i.e. N/123456789/system/0/Dc/Vebus/Power
+      if (!topic.startsWith(`N/${this.#id}/system/0/Dc`)) return
+
+      const topics: {[key: string]: { [key: string]: (val: number) => void }} = {
+        'Vebus': {
+          'Power': (val) => this.#socket?.emit('update', { consumption: val })
+        },
+        'Battery': {
+          'Soc': (val) => this.#socket?.emit('update', { battery: val })
+        },
+        'Pv': {
+          'Power': (val) => this.#socket?.emit('update', { solar: val })
+        },
+      }
+      
+      const device = topicParts[5]
+      const deviceTopic = topicParts[6]
+
+      const msg = JSON.parse(message)
+      const value = msg.value
+
+      if (topics[device] && topics[device][deviceTopic]) {
+        topics[device][deviceTopic](value)
+      }
+    })
+  }
+}
+
+const run = async () => {
+  const collector = new SolarDataCollector()
+  await collector.init()
+  collector.handleMessage()
+}
+
+run()
